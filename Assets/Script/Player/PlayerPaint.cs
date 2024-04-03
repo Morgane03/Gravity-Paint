8d219fa395bfcd88b689d8c88545f1d1688b9ba9
m v <morgane.voisin@e-artsup.net>
2024-04-03 11:30:37 +0200

    New effect to particle + clean player paint

--- Assets/Script/Player/PlayerPaint.cs
+++ Assets/Script/Player/PlayerPaint.cs
@@ -3,36 +3,26 @@ using UnityEngine;
 
 public class PlayerPaint : MonoBehaviour
 {
-    //private Collider2D _collider2D;
-
-    //private Vector2 _size;
-    //private Vector2 _baseSize;
-
     private PlayerController _playerController;
     private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_collider2D = GetComponent<Collider2D>();
         _playerController = GetComponent<PlayerController>();
         _animator = GetComponent<Animator>();
-        //_size = _collider2D.bounds.size;
-        //_baseSize = _size;
         _playerController.PlayerIsPaintingEvent += Paint;
     }
 
     public void Paint()
     {
         _animator.SetBool("Paint", true);
-        //_size = new Vector2(5.5205f, _size.y);
         StartCoroutine(StopPaint());
     }
 
     private IEnumerator StopPaint()
     {
         yield return new WaitForSeconds(0.5f);
-        //_size = _baseSize;
         _animator.SetBool("Paint", false);
     }
 }
