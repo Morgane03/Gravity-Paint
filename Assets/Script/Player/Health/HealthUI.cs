93a2999d1a44bde25d854bdfbd906b460e4153a3
m v <morgane.voisin@e-artsup.net>
2024-04-03 08:30:40 +0200

    Finish UI health

--- Assets/Script/Player/Health/HealthUI.cs
+++ Assets/Script/Player/Health/HealthUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,9 @@ public class HealthUI : MonoBehaviour
     private Slider _healthSlider;
 
     [SerializeField]
+    private Slider _backHealthSlider;
+
+    [SerializeField]
     private int _updateDuration = 1;
 
     [SerializeField]
@@ -20,24 +24,33 @@ public class HealthUI : MonoBehaviour
     {
         _playerHealth = GetComponent<PlayerHealth>();
         _healthSlider.maxValue = _playerHealth.MaxHealth;
+        _backHealthSlider.maxValue = _playerHealth.MaxHealth;
         _playerHealth.PlayerHealthChangedEvent += UpdateHealthUI;
     }
 
     public void UpdateHealthUI(int health)
     {
-        StartCoroutine(UpdateHealthSlider(health, _updateDuration));
-    }
-
-    private IEnumerator UpdateHealthSlider(int health, int duration)
-    {
+        //StartCoroutine(UpdateHealthSlider(health, _updateDuration));
         float startFillAmount = _healthSlider.value;
-        float timer = 0;
+        float targetFillAmount = Mathf.Lerp(startFillAmount, health, _updateDuration); //Mathf.InverseLerp(0, _playerHealth.MaxHealth, health);
 
-        while (timer < duration)
-        {
-            timer += Time.deltaTime;
-            _healthSlider.value = Mathf.Lerp(startFillAmount, health, _updateCurve.Evaluate(timer / duration));
-            yield return null;
-        }
+
+        DOTween.Sequence()
+            .Append(_healthSlider.DOValue(targetFillAmount, _updateDuration / 2f).SetEase(_updateCurve))
+            .AppendInterval(0.5f)
+            .Append(_backHealthSlider.DOValue(targetFillAmount, _updateDuration / 2f).SetEase(_updateCurve));
     }
+
+    //private IEnumerator UpdateHealthSlider(int health, int duration)
+    //{
+    //    float startFillAmount = _healthSlider.value;
+    //    float timer = 0;
+
+    //    while (timer < duration)
+    //    {
+    //        timer += Time.deltaTime;
+    //        _healthSlider.value = Mathf.Lerp(startFillAmount, health, _updateCurve.Evaluate(timer / duration));
+    //        yield return null;
+    //    }
+    //}
 }
