8e580ce073740c294bbb07f237f674c872752261
Eva291204 <eva.fricaud@e-artsup.net>
2024-04-04 16:38:09 +0200

    sound mananger

--- Assets/Script/Ennemi/EnnemiAnimation.cs
+++ Assets/Script/Ennemi/EnnemiAnimation.cs
@@ -29,6 +29,7 @@ public class EnnemiAnimation : MonoBehaviour
         _animator.SetBool("Attack", false);
         _animator.SetBool("Walk", false);
         _particles.Play();
+        SoundManager.Instance.GlitterEffect();
 
         StartCoroutine(WaitChangeAnimation());
     }
